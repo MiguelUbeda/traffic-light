759892289bf51ef0630a91b877a167274a50a072
Leeugim <103248971+Leeugim@users.noreply.github.com>
2022-06-24 20:57:30 +0200

    New features

--- src/js/component/traffic_light.jsx
+++ src/js/component/traffic_light.jsx
@@ -7,9 +7,9 @@ const Semáforo = () => {
     <>
     <div class="container top bg-dark d-grid justify-content-center"></div>
     <div class="container semáforo bg-dark d-grid justify-content-center">
-        <div style={{boxShadow: color === "red" ? "0px 0px 4px 4px #d7de50" : 'none'}}class="red light bg-danger rounded-circle" onClick={()=> setColor("red")}></div>
-        <div style={{boxShadow: color === "yellow" ? "0px 0px 5px 5px #d7de50" : 'none'}}class="yellow light bg-warning rounded-circle" onClick={()=> setColor("yellow")}></div>
-        <div style={{boxShadow: color === "green" ? "0px 0px 5px 5px #d7de50" : 'none'}}class="green light bg-success rounded-circle" onClick={()=> setColor("green")}></div>
+        <div style={{boxShadow: color === "red" ? "0px 0px 4px 4px #d7de50" : 'none'}}class="red light bg-danger rounded-circle mt-1" onClick={()=> color === "red" ? setColor("none") : setColor("red")}></div>
+        <div style={{boxShadow: color === "yellow" ? "0px 0px 5px 5px #d7de50" : 'none'}}class="yellow light bg-warning rounded-circle" onClick={()=> color === "yellow" ? setColor("none") :setColor("yellow")}></div>
+        <div style={{boxShadow: color === "green" ? "0px 0px 5px 5px #d7de50" : 'none'}}class="green light bg-success rounded-circle" onClick={()=> color === "green" ? setColor("none") :setColor("green")}></div>
     </div>
     </>
     )
